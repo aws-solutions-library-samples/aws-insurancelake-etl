2a05963099fc03dc0dd4ac548f77c8689806fb95
Cory Visi <117751550+coryvisi@users.noreply.github.com>
2023-09-23 01:02:38 -0400

    Update package name for PyPI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fp:
     long_description = fp.read()
 
 setuptools.setup(
-    name="aws_insurancelake_etl",
+    name="aws-insurancelake-etl",
     version="2.3.0",
     description="A CDK Python app for deploying ETL jobs that build an Insurance Lake in AWS",
     long_description=long_description,
