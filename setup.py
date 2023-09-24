fc6bdc86e822ebad76a9387bc8a8aa84268e646e
Cory Visi <117751550+coryvisi@users.noreply.github.com>
2023-09-24 19:57:36 -0400

    Package and workflow updates

--- setup.py
+++ setup.py
@@ -7,18 +7,19 @@ with open("README.md") as fp:
 
 setuptools.setup(
     name="aws-insurancelake-etl",
-    version="2.3.0",
+    version="2.4.0",
     description="A CDK Python app for deploying ETL jobs that build an Insurance Lake in AWS",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/aws-samples/aws-cdk-insurancelake-etl",
     author="Cory Visi <cvisi@amazon.com>, Ratnadeep Bardhan Roy <rdbroy@amazon.com>, Isaiah Grant <igrant@2ndwatch.com>, Ravi Itha <itharav@amazon.com>, Zahid Muhammad Ali <zhidli@amazon.com>",
     packages=setuptools.find_packages(),
     install_requires=[
         "aws-cdk-lib>=2.80.0",
         "constructs>=10.1.0",
     ],
-    python_requires=">=3.7",
-    keywords='aws_insurancelake_etl aws cdk insurance datalake etl glue spark',
+    python_requires=">=3.9",
+    keywords='aws-insurancelake-etl aws cdk insurance datalake etl glue spark python pyspark',
     license='MIT-0',
     classifiers=[
         "Development Status :: 4 - Beta",
