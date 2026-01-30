69fe461e4bbe5a1300a358f5e882601298a17290
Cory Visi <117751550+coryvisi@users.noreply.github.com>
2026-01-30 12:15:58 -0500

    Fix to copyright language

--- resources/datatransform_premiumdemo.py
+++ resources/datatransform_premiumdemo.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
+# Copyright Amazon.com and its affiliates; all rights reserved. This file is Amazon Web Services Content and may not be duplicated or distributed without permission.
 # SPDX-License-Identifier: MIT-0
 from decimal import Decimal
 import sys
